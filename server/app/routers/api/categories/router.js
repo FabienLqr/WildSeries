6f351324a6d93dcca4f331e312dcc7bd25f1faf9
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-27 17:39:20 +0200

    Quest 04: Done

--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -6,19 +6,7 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import item-related actions
-const { browse, read } = require("../../../controllers/programAction");
-
-// Route to get a list of programs
-router.get("/", browse);
-
-// Route to get one program
-router.get("/:id", read);
-
-// Route to get category
-const categoriesRouter = require("../../../controllers/categoryAction");
-
-router.use("/categories", categoriesRouter);
+/* Here you code */
 
 /* ************************************************************************* */
 
